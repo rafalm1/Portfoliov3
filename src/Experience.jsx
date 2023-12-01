b2104fbefc0f0015174b655ad22f14966c5c3811
Rafal Malyszko <rafal.malyszko@gtv.com.pl>
2023-12-01 19:29:14 +0100

    Add macbook model

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,19 +5,17 @@ import {
   ContactShadows,
   Html,
   PerspectiveCamera,
-} from '@react-three/drei'
+} from "@react-three/drei";
 
-import { useThree } from '@react-three/fiber'
+import { useThree } from "@react-three/fiber";
 
 export default function Experience() {
-  const computer = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
-  )
+  const computer = useGLTF("/mac-draco.glb");
 
-  const { viewport } = useThree()
-  const isMobile = window.innerWidth < 768;
+  const { viewport } = useThree();
   const responsiveRatio = viewport.width / 6;
-  const laptopScaleRatio = Math.max(0.5, Math.min(0.9 * responsiveRatio, 0.9))
+  const laptopScaleRatio =
+    Math.max(0.5, Math.min(0.9 * responsiveRatio, 0.9)) * 0.3;
 
   return (
     <>
@@ -25,30 +23,21 @@ export default function Experience() {
         <Environment preset="city" />
         <PresentationControls
           global
-          polar={[-0.4, 0.2]}
-          azimuth={[-0.5, 1.25]}
+          polar={[-0.2, 0.5]}
+          azimuth={[-0.5, 0.9]}
           config={{ mass: 4, tension: 400 }}
           snap={{ mass: 2, tension: 400 }}
         >
-          <rectAreaLight
-            width={2.5}
-            height={1.65}
-            intensity={65}
-            color={'#ff6900'}
-            rotation={[-0.1, Math.PI, 0]}
-            position={[0, 0.55, -1.15]}
-          />
           <primitive
             object={computer.scene}
-            position-y={isMobile ? -0.75 : - 1.25}
             scale={[laptopScaleRatio, laptopScaleRatio, laptopScaleRatio]}
           >
             <Html
               transform
               wrapperClass="htmlSection"
-              distanceFactor={0.93}
-              position={[0, 1.56, -1.4]}
-              rotation-x={-0.256}
+              distanceFactor={2.95}
+              position={[0, 3.025, 0.35]}
+              rotation-x={0}
             >
               <iframe src="https://rafalmalyszkov2.netlify.app/" />
             </Html>
@@ -57,5 +46,6 @@ export default function Experience() {
         <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
       </PerspectiveCamera>
     </>
-  )
+  );
 }
+1;
